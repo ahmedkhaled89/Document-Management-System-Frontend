074afbc7f35feb9edf9e8eddf200ffff96c3b9cb
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-03 23:28:53 +0300

    Add route to register

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import Register from './Components/Register';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/register' element={<Register />} />
+        <Route path='/comingSoon' element={<h1>Coming soon</h1>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
