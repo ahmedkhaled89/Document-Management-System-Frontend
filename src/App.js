c0a54774d4ca6ab32c3a1daa24afdbfb0ab112d5
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-04 13:33:53 +0300

    Add routes to login and home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Register from './Components/Register';
+import Home from './Components/Home';
+import Login from './Components/Lgoin';
 
 function App() {
   return (
     <div className='App'>
       <Routes>
         <Route path='/register' element={<Register />} />
-        <Route path='/comingSoon' element={<h1>Coming soon</h1>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </div>
   );
